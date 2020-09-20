e6fc335e056c2ea85f6daf830bf88949175c9b0f
DimonKeval <kevaldima@gmail.com>
2020-09-20 12:09:48 +0200

    function in JS commit

--- skrypt.js
+++ skrypt.js
@@ -1,13 +1,24 @@
 let gender = 'female';
-let age = 33;
+let age = 59;
 let emAge;
-if (gender === 'male'){
-console.log('Wiek emerytalny dla mężczyzny wynosi 65 lat.');
-emAge = 65;
-} else if (gender === 'female'){
-console.log('Wiek emerytalny dla kobiety wynosi 60 lat.');
-emAge = 60;
+
+ const emeretalnyWiek = (gender) =>{
+        if (gender === 'male'){
+            console.log('Wiek emerytalny dla mężczyzny wynosi 65 lat.');
+            return 65;
+        } else if (gender === 'female'){
+            console.log('Wiek emerytalny dla kobiety wynosi 60 lat.');
+            return 60;
+        }
+   }
+
+
+ const licznik = function liczenie(emAge){
+       for( ; age <= emAge; age++){
+           console.log('w wieku ' + age + ' lat zostaje do emeretury '+ (emAge - age) +' lat');
+       }
 }
-for( ; age <= emAge; age++){
-    console.log('w wieku ' + age + ' lat zostaje do emeretury '+ (emAge - age) +' lat');
-}
+
+emAge = emeretalnyWiek(gender);
+
+licznik(emAge);
